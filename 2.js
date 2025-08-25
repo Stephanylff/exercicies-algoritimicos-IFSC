1efb675dda778dea545f268a43f29890e14984d9
LeiMel <leizanemelo@gmail.com>
2025-08-25 16:45:23 -0300

    resolução da questão 2

---
+++ 2.js
@@ -0,0 +1,10 @@
+var salary, percentage, new_salary;
+undefined
+salary = prompt("3000");
+'3000'
+percentage = prompt("enter your percentage");
+'2'
+new_salary = salary * (1+percentage/100)
+3060
+console.log ("new salary:", new_salary)
+1 new salary: 3060
